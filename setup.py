e9d5f053ff468296558f7b9a288e7012939b109a
reinvantveer <rein.van.t.veer@geodan.nl>
2019-10-11 10:16:10 +0200

    fix dash issue?

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 import setuptools
 
 setuptools.setup(
-    name="point_cloud_colorize",
-    version="0.0.2",
+    name="point-cloud-colorize",
+    version="0.0.3",
     author="Chris Lucas, Arno Timmer, Rein van 't Veer",
     author_email="chris.lucas@geodan.nl, arno.timmer@geodan.nl, rein@geodan.nl",
     description="Point cloud colorization library",
     long_description="Colorizes point cloud .las or .laz files using aerial photography",
     long_description_content_type="text/markdown",
     url="https://github.com/Geodan/point-cloud-colorize",
-    packages=['point-cloud-colorize'],
+    packages=['point_cloud_colorize'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: Windows",
